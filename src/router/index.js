212f7ca32edbedb0af74c798fda1a3f7370168b7
DanielHajduk98 <danhajduk@protonmail.com>
2021-03-21 17:05:26 +0100

    :sparkles: Added route guard for day.
    Route guard checks if route date param is valid, before today or is today. Redirects to /404 or /dashboard in error cases

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import store from "@/store";
+import { parseISO, isValid, isBefore, isToday } from "date-fns";
 
 Vue.use(VueRouter);
 
@@ -19,14 +20,31 @@ const routes = [
     name: "Day",
     component: () => import("../views/Day.vue"),
     beforeEnter: (to, from, next) => {
-      store.commit("loader/setLoading", true, { root: true });
-      next();
+      const date = parseISO(to.params.date);
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+
+      if (isValid(date) && isBefore(date, today)) {
+        store.commit("loader/setLoading", true, { root: true });
+        next();
+      } else if (isToday(date)) {
+        next("/");
+      } else {
+        next("/404");
+      }
     }
   },
   {
     path: "/calendar",
     name: "Calendar",
     component: () => import("../views/Calendar.vue")
+  },
+  {
+    path: "/404"
+  },
+  {
+    path: "*",
+    redirect: "/404"
   }
 ];
 
