c98a2e0ace31146396f2d3876336be645e08d4f9
DanielHajduk98 <danhajduk@protonmail.com>
2021-03-23 19:55:08 +0100

    :sparkles: Added scroll to top fab button

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,37 @@
           indeterminate
         ></v-progress-circular>
       </v-overlay>
+
+      <v-container class="fab-container">
+        <v-fade-transition>
+          <v-btn
+            v-if="scrollPosition >= 700"
+            @click="scrollToTop"
+            class="fab"
+            color="primary"
+            elevation="2"
+            fab
+          >
+            <v-icon>mdi-arrow-up-bold</v-icon>
+          </v-btn>
+        </v-fade-transition>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+
 export default {
   name: "App",
   components: { Navbar },
+
+  data() {
+    return {
+      scrollPosition: null
+    };
+  },
 
   watch: {
     isLoading: function(newValue) {
@@ -38,6 +60,20 @@ export default {
 
   beforeCreate() {
     this.$vuetify.theme.dark = true;
+  },
+
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+
+    scrollToTop() {
+      this.$vuetify.goTo(0);
+    }
   },
 
   computed: {
@@ -64,5 +100,22 @@ body.no-scroll {
 
 .loader--true {
   height: calc(100vh - 50px) !important;
+}
+
+.fab-container {
+  position: sticky;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  pointer-events: none;
+  justify-content: end;
+
+  & > .fab {
+    pointer-events: auto;
+  }
+}
+
+.v-main__wrap {
+  position: relative;
 }
 </style>
